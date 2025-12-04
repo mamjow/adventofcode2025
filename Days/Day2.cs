df5f2ebb9b8addbac9f47b4e43cd974a7c37db11
M.J. <mj.moshiri@gmail.com>
2025-12-04 19:00:28 +0100

    day 2 .

--- Days/Day2.cs
+++ Days/Day2.cs
@@ -1,4 +1,5 @@
-﻿using App;
+﻿using System.Text.RegularExpressions;
+using App;
 
 namespace Days
 {
@@ -25,7 +26,21 @@ namespace Days
 
         public string SolvePartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            var data = input[0].Split(",");
+            long counter = 0;
+            foreach (var idRange in data)
+            {
+                var ids = idRange.Split("-");
+                _ = long.TryParse(ids[0], out long min);
+                _ = long.TryParse(ids[1], out long max);
+
+                for (long i = min; i <= max; i++)
+                {
+                    counter += PatternsFoundManyTime(i) ? i : 0;
+                }
+
+            }
+            return $"{counter}";
         }
 
         public static bool PatternsFoundTwice(long sampleId)
@@ -45,6 +60,19 @@ namespace Days
 
         public static bool PatternsFoundManyTime(long sampleId)
         {
+            var stringifiedId = sampleId.ToString();
+            
+            for (var i = 1; i <= stringifiedId.Length/2; i++)
+            {
+                // at least 2 so we nex just to check pattersn to half
+                var sub = stringifiedId[..i];
+                var occurance = Regex.Matches(stringifiedId, sub).Count();;
+                // ocurance times length musb be whole string size
+                if(occurance * sub.Length == stringifiedId.Length)
+                {
+                    return true;
+                }      
+            }
 
             return false;
         }
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 | Challenge | Solution | Language | Progress        |
 |---|---|---|---|
 |[Day 1: Secret Entrance](https://adventofcode.com/2025/day/1)    | [🔑](https://github.com/mamjow/adventofcode2025/blob/main/Days/Day1.cs)   | C#       | :star: :star:   |   
+|[Day 2: Gift Shop](https://adventofcode.com/2025/day/2)    | [🔑](https://github.com/mamjow/adventofcode2025/blob/main/Days/Day2.cs)   | C#       | :star: :star:   |   
